70cac84af8ab37ff54d9cd2233dee4ec975fb6df
J-dance <dance.jojo@gmail.com>
2023-01-26 17:36:13 +0000

    added font and font-splash screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import AuthContainer from "./components/AuthContainer";
 import { UserContext } from './assets/context'
 import { User } from './assets/types';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [userData, setUserData] = useState<User>({
@@ -12,6 +17,20 @@ export default function App() {
     authenticated: false,
     recipes: []
   });
+
+  const [fontsLoaded] = useFonts({
+    'Donegal-One': require('./assets/fonts/DonegalOne-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <UserContext.Provider value= {{ userData, setUserData }}>
--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -1,28 +1,56 @@
-import React from 'react'
-import Page from '../components/Page';
-import { Text, Button } from 'react-native';
-import { useUserContext } from '../assets/context';
+import React from "react";
+import Page from "../components/Page";
+import { Button, StyleSheet, View, Text } from "react-native";
+import { useUserContext } from "../assets/context";
+import { SvgXml } from "react-native-svg";
+import { xml as bookIconXml } from "../assets/icons/book.js";
 
 const Landing = () => {
   const { setUserData } = useUserContext();
   const loginUser = () => {
     setUserData({
-      name: '',
-      id: '',
+      name: "",
+      id: "",
       authenticated: true,
-      recipes: []
-    })
+      recipes: [],
+    });
   };
   return (
     <Page>
+      <SvgXml xml={bookIconXml} width={100} height={100} />
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText1}>Recipe-</Text>
+        <Text style={styles.titleText2}>Me</Text>
+      </View>
       <Button
         onPress={loginUser}
         title="Log in"
-        color="#D44D5C"
+        color="#FFFFFF"
         accessibilityLabel="Log in to your account"
       />
     </Page>
-  )
-}
+  );
+};
 
-export default Landing
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    borderColor: '#FFFFFF',
+    width: '80%',
+    borderWidth: 2,
+    paddingVertical: 10
+  },
+  titleText1: {
+    fontFamily: 'Donegal-One',
+    fontSize: 42,
+    color: '#FFFFFF'
+  },
+  titleText2: {
+    fontFamily: 'Donegal-One',
+    fontSize: 42,
+    color: 'orange'
+  }
+})
+
+export default Landing;
